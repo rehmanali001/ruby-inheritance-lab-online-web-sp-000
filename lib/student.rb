e00bf84b059c4c97d56b5b0d0eece195f18352e6
Rehman Ali <rehmanali001@gmail.com>
2019-08-14 18:53:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
-
+class Student < User
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(name)
+    @knowledge << name
+  end 
+  
+  def knowledge
+    return @knowledge
+  end 
 end
